b8c66acf2af4778318fb64164645d85e91b2dd3e
TRex612 <90751734+TRex612@users.noreply.github.com>
2023-11-22 16:22:03 +0700

    add method InputNum (#2)
    Co-authored-by: TRex612 <poochit.moonkààaew@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+
+
 public class Main {
+
+
+    static int InputNum(int[] x,int i){
+        Scanner IntNum = new Scanner(System.in);
+        System.out.print("Enter integer : ");
+        x[i] = IntNum.nextInt() ;
+        return x[i];
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner Number = new Scanner(System.in);
+        System.out.print("Enter number of integers : ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int SizeNumber = Number.nextInt();
+        int[] arr = new int[SizeNumber];
+
+        for (int j = 0 ;j<SizeNumber;j++){
+            arr[j] = InputNum(arr,j);
         }
+        Arrays.sort(arr);
+        System.out.println("Result");
+        for (int k = 0 ;k < SizeNumber;k++){
+            System.out.println(arr[k]);
+        }
+
+
     }
 }
